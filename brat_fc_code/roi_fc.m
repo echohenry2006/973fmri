d92c00f5fe1aa42083f3f097f5f4a73c7c627f5e
echohenry2006 <echohenry2006@gmail.com>
2015-06-25 23:20:36 +0800

    some change
    低版本的matlab 无法用fullfile直接作用cells
    a=dir()
    {a.name}

--- brat_fc_code/roi_fc.m
+++ brat_fc_code/roi_fc.m
@@ -2,18 +2,26 @@ clc;
 clear;
 load jobman.mat
 
+nii_dir='/DATA/231/sz_fmri/DATA_prep_3mm/with_GR_s';
+
 jobman.roi2roi=0;
 jobman.roi2wb=1;
+
 jobman.mask={'/DATA/231/sz_fmri/PKU6/corr_map/test/ROIS/rThalamus_HO_AAL_3mm.nii'};
-jobman.rois= {'/DATA/231/sz_fmri/PKU6/corr_map/test/ROIS/brain_lobe_6.nii'};
+jobman.rois= {'/DATA/238/yyang/MatlabToolbox/Brat/template/fmaskEPI_V3mm.nii'};
+
 jobman.input_nifti.nm_pos=1;
 jobman.input_nifti.filetype='*.nii.gz';
-
 jobman.input_nifti.is4d=1;
-jobman.out_dir={'/DATA/231/sz_fmri/PKU6/corr_map/test/'};
+jobman.out_dir={'/DATA/231/sz_fmri/PKU6/corr_map/Thu_Brain_partial'};
 jobman.outputP=0;
 
-jobman.input_nifti.dirs={'/DATA/231/sz_fmri/DATA_prep_3mm/with_GR_s/SZ_01_0068/'};
+tmp=dir(fullfile(nii_dir));
+tmp=tmp(cell2mat({tmp.isdir}));
+tmp=tmp(~strcmp({tmp.name},'.') & ~strcmp({tmp.name},'..'));
+nii_name={tmp.name}';
+nii_file=cellfun(@(x) fullfile(nii_dir,x),nii_name,'UniformOutput',false);
+jobman.input_nifti.dirs=nii_file;
 
-brat_roi_script(jobman);
+%brat_roi_script(jobman);
 brat_roi_script_partial(jobman);
