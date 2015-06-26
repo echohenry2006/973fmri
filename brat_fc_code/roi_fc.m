64d2604304142e4c8d9da46ca6127b14a4a35706
echohenry2006 <echohenry2006@gmail.com>
2015-06-26 15:13:32 +0800

    revise
    meta

--- brat_fc_code/roi_fc.m
+++ brat_fc_code/roi_fc.m
@@ -2,18 +2,18 @@ clc;
 clear;
 load jobman.mat
 
-nii_dir='/DATA/231/sz_fmri/DATA_prep_3mm/with_GR_s';
+nii_dir='/DATA/234/sz_fmri/DATA_prep_3mm/with_GR_s';
 
 jobman.roi2roi=0;
 jobman.roi2wb=1;
 
-jobman.mask={'/DATA/231/sz_fmri/PKU6/corr_map/test/ROIS/rThalamus_HO_AAL_3mm.nii'};
-jobman.rois= {'/DATA/238/yyang/MatlabToolbox/Brat/template/fmaskEPI_V3mm.nii'};
+jobman.rois={'/DATA/231/sz_fmri/PKU6/corr_map/test/ROIS/brain_lobe_6.nii'};
+jobman.mask= {'/DATA/238/yyang/MatlabToolbox/Brat/template/fmaskEPI_V3mm.nii'};
 
 jobman.input_nifti.nm_pos=1;
 jobman.input_nifti.filetype='*.nii.gz';
 jobman.input_nifti.is4d=1;
-jobman.out_dir={'/DATA/231/sz_fmri/PKU6/corr_map/Thu_Brain_partial'};
+jobman.out_dir={'/DATA/238/yyang/workspace/973_FMRI/234_corr_map/Thu_Brain_partial'};
 jobman.outputP=0;
 
 tmp=dir(fullfile(nii_dir));
--- brat_meta_code/meta.m
+++ brat_meta_code/meta.m
@@ -2,31 +2,34 @@
 param1={'one_sample_Ttest_NC','one_sample_Ttest_SZ','Ttest'};
 param2={'001','002','003','004','005','006'};
 
-for i=1:1;%length(param1)
-    for j=1:1%length(param2)
+for i=1:length(param1)
+    for j=1:length(param2)
 %         clear;
 %         clc;
         
         load jobman.mat
         input_type = {'voxel'};
         
-        jobman.out_dir= {strcat('/DATA/234/sz_fmri/ALE_AH/WITH_GR/Thu_Brain/',param1{i},'/',param2{j},'/meta/')};
+        jobman.out_dir= {strcat('/DATA/238/yyang/workspace/973_FMRI/Thu_Brain/',param1{i},'/',param2{j},'/meta/')};
         
-        jobman.mask= {'/DATA/234/sz_fmri/ALE_AH/WITH_GR/Thu_Brain/rThalamus_HO_AAL_3mm.nii'};
+        jobman.mask= {'/DATA/238/yyang/workspace/973_FMRI/Thu_Brain/rThalamus_HO_AAL_3mm.nii'};
+        jobman.input_nifti.filetype='*.img';
+        jobman.input_nifti.dirs = {strcat('/DATA/238/yyang/workspace/973_FMRI/Thu_Brain/',param1{i},'/',param2{j},'/meta/')};
         
-        jobman.input_nifti.dirs = {strcat('/DATA/234/sz_fmri/ALE_AH/WITH_GR/Thu_Brain/',param1{i},'/',param2{j},'/meta/')};
-        
-        if( exist( strcat( '/DATA/234/sz_fmri/ALE_AH/WITH_GR/Thu_Brain/',param1{i},'/oo_info_ttest.xlsx'),'file') )
+        switch param1{i}
+		case 'one_sample_Ttest_NC'
+			xls='/oo_info_ttest_NC.xlsx'
+     		case 'one_sample_Ttest_SZ'
+			xls='/oo_info_ttest_SZ.xlsx'
+		case 'Ttest'
+			xls='/oo_info_ttest2.xlsx'
             
-            jobman.num_subjs_tbl= {strcat('/DATA/234/sz_fmri/ALE_AH/WITH_GR/Thu_Brain/',param1{i},'/oo_info_ttest.xlsx')};
-        else
-            
-            jobman.num_subjs_tbl= {strcat('/DATA/234/sz_fmri/ALE_AH/WITH_GR/Thu_Brain/',param1{i},'/oo_info_ttest2.xlsx')};
         end
-        
-        
+        jobman.num_subjs_tbl= {strcat('/DATA/238/yyang/workspace/973_FMRI/Thu_Brain/',param1{i},xls)};
         
         brat_ibma(jobman, input_type);
         
     end
 end
+disp('-------------------------------DONE!!---------------------------')
+exit;
